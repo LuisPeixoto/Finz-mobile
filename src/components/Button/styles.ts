5bc25941c4195a595ef62c20028e7cfb8a690250
LuisPeixoto <luispeixoto@pq.uenf.br>
2021-08-11 18:59:46 -0300

    feat: button component

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native'
+import { RectButton } from 'react-native-gesture-handler'
+
+export const Container = styled(RectButton)`
+  width: 100%;
+  height: 60px;
+  background: #4946fa;
+  border-radius: 5px;
+  justify-content: center;
+  align-items: center;
+  margin-top: 16px;
+`
+export const ButtonText = styled.Text`
+  font-family: 'Lato-Regular';
+  color: #fff;
+  font-size: 16px;
+`
