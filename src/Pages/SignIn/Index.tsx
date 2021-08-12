f747456b682e3fe03ab49575f8fb7d7bfaca0b48
LuisPeixoto <luispeixoto@pq.uenf.br>
2021-08-12 16:07:21 -0300

    feat:  create new account and forgot password buttons

--- src/Pages/SignIn/Index.tsx
+++ src/Pages/SignIn/Index.tsx
@@ -1,25 +1,64 @@
 import React from 'react'
-import { Container, Title } from './styles'
-import { Image } from 'react-native'
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+  ScrollView,
+} from 'react-native'
 import logoImg from '../../assets/logo.png'
 import Input from '../../components/Input'
 import Button from '../../components/Button'
 
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles'
+import Icon from 'react-native-vector-icons/Feather'
+
 const SignIn: React.FunctionComponent = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
-      <Title>Mantenha-se conectado</Title>
-      <Input name="email" icon="mail" placeholder="E-mail" />
-      <Input name="password" icon="lock" placeholder="Senha" />
-      <Button
-        onPress={() => {
-          console.log('teste')
-        }}
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
       >
-        Entrar
-      </Button>
-    </Container>
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Mantenha-se conectado</Title>
+            </View>
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button
+              onPress={() => {
+                console.log('teste')
+              }}
+            >
+              Entrar
+            </Button>
+            <ForgotPassword onPress={() => {}}>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <CreateAccountButton onPress={() => {}}>
+        <Icon name="log-in" size={20} color="#8584F7" />
+        <CreateAccountButtonText>Criar Conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   )
 }
 
--- src/Pages/SignIn/styles.ts
+++ src/Pages/SignIn/styles.ts
@@ -10,6 +10,34 @@ export const Container = styled.View`
 export const Title = styled.Text`
   font-family: 'Lato-Regular';
   color: #656565;
-  font-size: 24px;
-  margin-top: 32px;
+  font-size: 21px;
+  margin: 32px 0;
+`
+export const ForgotPassword = styled.TouchableOpacity`
+  margin-top: 24px;
+`
+
+export const ForgotPasswordText = styled.Text`
+  font-family: 'Lato-Regular';
+  color: #656565;
+  font-size: 16px;
+`
+export const CreateAccountButton = styled.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: #fff;
+  border-top-width: 1px;
+  border-color: #efefef;
+  padding: 16px 0;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`
+export const CreateAccountButtonText = styled.Text`
+  color: #8584f7;
+  font-family: 'Lato-Regular';
+  font-size: 18px;
+  margin-left: 16px;
 `
