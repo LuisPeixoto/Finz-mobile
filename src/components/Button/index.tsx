41b08c0ded8e84880c39724e26dfa9efba0d0736
LuisPeixoto <luispeixoto@pq.uenf.br>
2021-08-16 20:03:15 -0300

    feat: color, text Color and border in button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,17 +1,26 @@
 import React from 'react'
-import { Container, ButtonText } from './styles'
+import { Container, ButtonText, ContainerBorder } from './styles'
 import { RectButtonProperties } from 'react-native-gesture-handler'
 
 interface ButtonProps extends RectButtonProperties {
   children: string
+  color?: string
+  textColor?: string
+  hasBorder?: boolean
+  icon?: string
 }
 const Button: React.FunctionComponent<ButtonProps> = ({
   children,
+  color,
+  textColor,
+  hasBorder,
   ...rest
 }) => {
   return (
-    <Container {...rest}>
-      <ButtonText>{children}</ButtonText>
+    <Container hasBorder={hasBorder} color={color} {...rest}>
+      <ContainerBorder hasBorder={hasBorder} color={color}>
+        <ButtonText textColor={textColor}>{children}</ButtonText>
+      </ContainerBorder>
     </Container>
   )
 }
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,17 +1,55 @@
-import styled from 'styled-components/native'
+import styled, { css } from 'styled-components/native'
 import { RectButton } from 'react-native-gesture-handler'
 
-export const Container = styled(RectButton)`
+interface ButtonProps {
+  color?: string
+  textColor?: string
+  hasBorder?: boolean
+}
+
+export const Container = styled(RectButton)<ButtonProps>`
   width: 100%;
   height: 60px;
+
   background: #4946fa;
+  ${(props) =>
+    !!props.color &&
+    css`
+      background: ${props.color};
+    `}
   border-radius: 5px;
   justify-content: center;
   align-items: center;
   margin-top: 16px;
+
+  ${(props) =>
+    props.hasBorder &&
+    css`
+      background: transparent;
+    `}
 `
-export const ButtonText = styled.Text`
+
+export const ContainerBorder = styled.View<ButtonProps>`
+  ${(props) =>
+    props.hasBorder &&
+    css`
+      width: 100%;
+      height: 100%;
+      background: transparent;
+      border-width: 2px;
+      border-color: #ccc;
+      border-radius: 5px;
+      justify-content: center;
+      align-items: center;
+    `}
+`
+export const ButtonText = styled.Text<ButtonProps>`
   font-family: 'Lato-Regular';
   color: #fff;
-  font-size: 16px;
+  ${(props) =>
+    !!props.textColor &&
+    css`
+      color: ${props.textColor};
+    `}
+  font-size: 18px;
 `
