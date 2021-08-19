39eb02e44f88bf20bc3ba51a4ad9e0a14d910bd6
LuisPeixoto <luispeixoto@pq.uenf.br>
2021-08-19 16:26:46 -0300

    feat: change status bar color

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { Button, Text, View } from 'react-native'
+import { StatusBar } from 'react-native'
 import { useAuth } from '../../hooks/auth'
 
 const Home: React.FunctionComponent = () => {
   const { signOut } = useAuth()
   return (
-    <View>
-      <Button title="Sair" onPress={signOut} />
-    </View>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#4946fa" />
+
+      {/* <Button title="Sair" onPress={signOut} /> */}
+    </>
   )
 }
 
